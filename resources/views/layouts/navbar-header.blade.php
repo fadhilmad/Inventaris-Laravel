bb38770dcf5c9d9ff20fbb47ba35e6510af5e075
Ignatius Bagas K <abbbagas@gmail.com>
2021-09-24 12:18:18 +0700

    Update navbar-header.blade.php

--- resources/views/layouts/navbar-header.blade.php
+++ resources/views/layouts/navbar-header.blade.php
@@ -8,10 +8,10 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title>Sistem Informasi Inventaris Unipma</title>
 </head>
 <body>
-
+{{--Navbar--}}
 <nav class="navbar navbar-light bg-light">
     <div class="container-fluid">
         <div class="d-flex flex-column bd-highlight">
@@ -20,13 +20,36 @@
         </div>
     </div>
 </nav>
+{{--navbar--}}
+
+<div class="container" style="height: 78vh;">
+    <div class="d-flex justify-content-end">
+        <div class="card mt-4 mb-4" style="height: 70vh; width: 60vh">
+            <div class="card-body">
+                <h5 class="card-title text-center">LOGIN</h5>
+                <form class="text-center">
+                    <div class="mb-3 mt-5">
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="email">
+                    </div>
+                    <div class="mb-3">
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="password">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Masuk</button>
+                </form>
+                <hr class="mt-5">
+                <p class="text-muted">Hubungi kami apabila menemui kendala teknis melalui kontak berikut
+                </p>
+            </div>
+        </div>
+    </div>
+
+</div>
 
 <!-- Footer -->
 <footer class="text-center text-lg-start bg-light text-muted">
     <!-- Copyright -->
     <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
-        © 2021 Copyright:
-        <a class="text-reset fw-bold" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+        © 2021 Copyright: Universitas PGRI Madiun
     </div>
     <!-- Copyright -->
 </footer>
